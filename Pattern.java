c154645a1884333951b7f7113550e21555092113
priteshraman <mepriteshraman@gmail.com>
2016-06-04 16:53:06 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,19 @@
+public class Pattern{
+	public static void main(String args[]){
+		int i,j;
+		System.out.println("the required pattern is :");
+		for(i=1;i<=5;i++){
+			for(j=1;j<=i;j++){
+			System.out.print(j);
+			}
+		System.out.println();
+		}
+		for(i=5;i>=1;i--){
+			for(j=1;j<i;j++){
+			System.out.print(j);
+			}
+		System.out.println();
+		}
+	}
+      
+}
